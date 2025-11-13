37626f2f8e142866cd1dec3df9380c6320d8dc0c
lelmengit-ivan <ivan.danger.o4@gmail.com>
2025-11-13 14:36:20 +0300

    url added

--- frontend/static/config.js
+++ frontend/static/config.js
@@ -1,7 +1,7 @@
 // API Configuration
 const CONFIG = {
     // Auto-detect: uses local when running locally, Render when deployed
-    API_URL: window.location.origin + '/api'
+    API_URL: 'https://pharmacy-backened.onrender.com/api'
     
     // To force Render backend even when running locally, use this:
     // API_URL: 'https://pharmacy-system-pgkk.onrender.com/api'
